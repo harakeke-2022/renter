dcb3ff25dd97d42ba3521a4382401fec7a1263b3
Nikhita <k.nikhita@gmail.com>
2022-03-01 14:15:45 +1300

    add footer content

---
+++ client/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer () {
+  return (
+    <footer>
+     <p>General disclaimer: This is a public forum presenting user opinions on selected rental properties, and as such the views expressed are the subjective views of the Renter users and do not reflect the views or opinions of Renter. All third party trademarks, images and copyrights on this Site are used for the purposes of comparative advertising, criticism or review.</p>
+     <p>Copyright 2022 Renter. All Rights Reserved.</p>
+    </footer>
+  )
+}
